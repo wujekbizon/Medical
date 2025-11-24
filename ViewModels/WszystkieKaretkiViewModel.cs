6e0ee07ffc30ae871b7742d02604dde9b014c140
wujekbizon <wujekbizon@gmail.com>
2025-11-24 22:12:15 +0100

    Add AllKaretki fetching from db logic

--- ViewModels/WszystkieKaretkiViewModel.cs
+++ ViewModels/WszystkieKaretkiViewModel.cs
@@ -1,16 +1,31 @@
-﻿using System;
+﻿using Medical.ViewModels.Abstract;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Medical.ViewModels
 {
-    public class WszystkieKaretkiViewModel : WorkspaceViewModel
+    public class WszystkieKaretkiViewModel: WszystkieViewModel<dynamic>
     {
+        #region Lista
+        public override void Load()
+        {
+            List = new ObservableCollection<dynamic>
+                (
+                medicalEntities.Karetka.Where((k) => k.CzyAktywny == true).ToList()
+                );
+        }
+        #endregion
+
+        #region Konstruktor
         public WszystkieKaretkiViewModel()
         {
             base.DisplayName = "Karetki";
         }
+        #endregion
+
     }
 }
